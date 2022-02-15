078299d0a3904c60f794e2050ad59cc6f4b67b42
Peter <41585899+pbanigo@users.noreply.github.com>
2022-02-15 18:04:50 +0000

    Update game.js

--- game.js
+++ game.js
@@ -3,28 +3,13 @@ function computerPlay(){
 	//0-Rock, 1-Paper, 2-Scissors.
 	let num = Math.floor(Math.random()*3);
 	return num;
-
-	//let play;
-	/*switch(num) {
-  case 0:
-    play = "Rock";
-    break;
-  case 1:
-    play = "Paper";
-    break;
-  default:
-    play = "Scissors";
 }
-
-	return console.log(play);*/
-}
-//console.log(computerPlay());
 function round(playerSelection, computerSelection){
-	let player; 
+	let player;
 	switch(playerSelection) {
 		//I'd convert whatever is entered by player to lower case
   case "rock":
-    player = 0; //then convert to an integer using switc for easier comparison
+    player = 0; //then convert to an integer using switch for easier comparison
     break;
   case "paper":
     player = 1;
@@ -35,30 +20,38 @@ function round(playerSelection, computerSelection){
   default:
   	//player = prompt('Enter rock, paper or scissors');
     alert("Enter rock, paper or scissors"); //prompts to enter the right word if misspelt
-    //a regex can be used here if an imput form is built
+    //a regex can be used here if an im=nput form is built
+    //(rock|paper|scissors)
 		}
 	if ((player==0&&computerSelection==2)||(player==1&&computerSelection==0)||(player==2&&computerSelection==1)){
 		return "You Win!";// for winning scenarios, return message
 	} else if((player==2&&computerSelection==0)||(player==0&&computerSelection==1)||(player==1&&computerSelection==2)) {
 	return "You Lose!";// for losing scenarios return message
 	} else if((player==computerSelection)) {
-	return "It's a draw";// for losing scenarios return message
+	return "It's a draw";// if it wasn't won or lost then it's a draw
 	}  else {
-		return "Something went wrong";//if it wasn't won or lost then it's a draw
+		return "Something went wrong";//something actually went wrong :(
 	}
 }
 
 function game (){
-
+	//initialize scores for the full game
 	let playerScore = 0;
 	let computerScore = 0;
+	//loop for number of rounds needed
 	for (let i = 0; i < 3; i++) {
+	//prompt for player to enter option
 	let playerSelection = prompt('Enter rock, paper or scissors');
+	//convert to lower case for uniformity
 	playerSelection = playerSelection.toLowerCase();
+	//the computer's selection would be the result of the computerPlay() function
 	const computerSelection = computerPlay();
+	//Show what player and computer played
 	console.log(playerSelection);
 	console.log(computerSelection);
+	//save gameplay in a variable
 	let each = round(playerSelection, computerSelection);
+	//increment scores on each win or loss
 	switch(each) {
   		case "You Win!":
 		playerScore+=1;
@@ -67,25 +60,16 @@ function game (){
 		computerScore+=1;
     		break;
  		 case "It's a draw"||"Something went wrong":
-    		break;
+    		break;// end the loop
     	}
-	/*if (each=="You Win!"){
-		playerScore+=1;
-	} else if (each=="You Lose!") {
-		computerScore+=1;
-	} else {
-		continue;
-	}*/
-	//console.log('playerScore"and"computerScore');
-
-	//return each;
-
-	console.log(playerScore);
-	console.log(computerScore);
-	//if (playerScore>computerScore)
 	} 
+	//outside the loop, give results
+	if (playerScore>computerScore) {
+		return "You scored "+ playerScore + " Computer scored "+computerScore+" You Win!";
+	} else {
+		return "You scored "+ playerScore + " Computer scored "+computerScore+" You lost!";
+		
+	}
 
 }
-//console.log(computerSelection);
-//console.log(round(playerSelection, computerSelection));
 console.log(game());
