17e95b938314f41cc0d967b448ec283cf352a2ea
Peter <41585899+pbanigo@users.noreply.github.com>
2022-03-17 19:17:02 +0000

    Commented

--- gamedom.js
+++ gamedom.js
@@ -1,3 +1,4 @@
+//Pick out elements that would bbe manipulated using their ID's
 const inst = document.getElementById('instructions');
 const cPlay = document.getElementById('cPlay');
 const pPlay = document.getElementById('pPlay');
@@ -6,34 +7,36 @@ const cScore = document.getElementById('cScore');
 const currentPlay = document.getElementById('current');
 const currentRound = document.getElementById('thisRound');
 const game  = document.getElementById('game');
-const synth = window.speechSynthesis;
-
-
+const synth = window.speechSynthesis; //Added Voice synthesis
+//Initialised scores to Zero
 let playerScore = 0;
 let computerScore = 0;
-
-
-
+//playNow is called by the onClick on the Play button in the HTML
 function playNow(){
+	//Hiding unnecessary parts of the site till needed
 	currentRound.style.display = "none";
 	currentPlay.style.display = "none";
-	game.style.display = "block";
-	synth.speak(new SpeechSynthesisUtterance(inst.textContent));
+	game.style.display = "block"; //This displays the game div after the play button is clicked
+	synth.speak(new SpeechSynthesisUtterance(inst.textContent));//This reads any text in that element
+	//change button to a reload button after clicking & rewrites the text in it
 	document.getElementById('start').setAttribute('onclick','window.location.reload()');
 	document.getElementById('start').textContent = "Reload!";
+	//initialises the round
 	let thisRound = 1;
+	//selects all divs with class .item
 	const plays = document.querySelectorAll("div.item");
 	plays.forEach(play => {
 
 		play.addEventListener('click', function here(event) {
-			currentPlay.style.display = "flex";
-			currentRound.style.display = "block";
-			currentRound.textContent = `ROUND: ${thisRound}!`;
+			currentPlay.style.display = "flex";//brings up the hidden element
+			currentRound.style.display = "block";//brings up the hidden element
+			currentRound.textContent = `ROUND: ${thisRound}!`;//Displays round number
+			//for each click, saves the item ID in player selection
 			playerSelection = play.id;
-			computerSelection = computerPlay();
-			pPlay.textContent = `You played ${playerSelection.toUpperCase()}!`
-			round(playerSelection,computerSelection);
-			thisRound+=1;
+			computerSelection = computerPlay();//saves result of computerPlay function
+			pPlay.textContent = `You played ${playerSelection.toUpperCase()}!`//Displays what player played
+			round(playerSelection,computerSelection);//runs the round function using both values
+			thisRound+=1;//increments the round we are in
 
 
 		})
@@ -50,7 +53,9 @@ function computerPlay(){
 	//lets convert to answers this time
 	switch(num) {
 		case 0:
+		//shows what computer played
 		cPlay.textContent = 'Computer played ROCK!';
+		//converts the number to a usable string
 		comPlay="rock";
 		break;
 		case 1:
@@ -62,7 +67,7 @@ function computerPlay(){
 		comPlay="scissors";
 		break;
 	}
-	return comPlay;
+	return comPlay;//our result
 }
 function round(playerSelection, computerSelection){
 	if ((playerSelection==computerSelection)){
@@ -76,7 +81,7 @@ function round(playerSelection, computerSelection){
 		cScore.textContent = computerScore;
 		inst.textContent = "You Win!";
 		synth.speak(new SpeechSynthesisUtterance(`for ${currentRound.textContent} ${inst.textContent}`));
-		//return "win";
+		//return "win", write scores, increment player score;
 	} else if ((playerSelection=="scissors"&&computerSelection=="rock")||(playerSelection=="rock"&&computerSelection=="paper")||
 		(playerSelection=="paper"&&computerSelection=="scissors")) {
 		computerScore++;
@@ -84,15 +89,16 @@ function round(playerSelection, computerSelection){
 		cScore.textContent = computerScore;
 		inst.textContent = "Computer Wins!";
 		synth.speak(new SpeechSynthesisUtterance(`for ${currentRound.textContent} ${inst.textContent}`));
-		//return "lose";
+		//return "lose" write scores, increment computeer score;
 	} else {
-		inst.textContent =  "Something is wrong";
+		inst.textContent =  "Something is wrong";// why is this here?
 	}
 
 
 	if (playerScore==5 || computerScore==5){
+		//hides game area if winning condition met
 		game.style.display = "none";
-		document.getElementById('start').textContent = "Replay!";
+		document.getElementById('start').textContent = "Replay!";//change button to a replay button
 		if (playerScore>computerScore){
 			whoWon = "You won!"
 			message = "Congrats!"
